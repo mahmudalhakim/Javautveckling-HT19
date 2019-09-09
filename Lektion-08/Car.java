50b6a03c0a474b6f9236afb63534f65d048515db
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2019-09-09 12:16:08 +0200

    Lektion 8

---
+++ Lektion-08/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+    // Instansvariabler
+    int modelYear;
+    String modelName;
+    double price;
+    boolean sold;
+    char tecken;
+    Person agare; // Bilens ägare
+
+    // Klassvariabel
+    public static final double MOMS = 0.25;
+
+}
---
+++ Lektion-08/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+
+    // Instansvariabler
+    String name;
+    String personnummer;
+    int age; // onödig. Vi kan ta fram ålder från PN
+    double weight;
+}
